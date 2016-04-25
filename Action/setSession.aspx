310ef482a63103183ecc4a8a4f67a3cef9a65c51
alexloescher <alex.loescher@ingeniux.com>
2016-04-25 08:37:01 -0700

    Commit Solution

---
+++ Action/setSession.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Language="C#" %>
+<%
+    // Expected input: categoryType, categoryValue
+
+    // Function: sets the donor category value for the given category type overriding any previous value
+
+    string catType = Request["key"];
+    string catValue = Request["value"];
+
+    // Check for a valid category type (key) and then add its value to the donor profile
+    if (catType != null && catType.Length > 0)
+    {
+        Dictionary<string, string> donorCats = Session["donorCats"] as Dictionary<string, string>;
+        if (donorCats != null)
+        {
+            donorCats.Remove(catType);
+            donorCats.Add(catType, catValue);
+        }
+        else
+        {
+            donorCats = new Dictionary<string, string>();
+            donorCats.Add(catType, catValue);
+        }
+
+        Session["donorCats"] = donorCats;
+    }
+%>
---
+++ Models/FormInfoWrapper.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Ingeniux.Runtime.Models
+{
+	public class FormInfoWrapper
+	{
+		#region Static Members
+		private static StelterFormsDataContext dc;
+
+		static FormInfoWrapper()
+		{
+			dc = new StelterFormsDataContext();
+		}
+		#endregion
+
+		public void AddFormSubmission(String CustomerName, String ReferringUrl, String FormName, String PageName, String FirstName, String LastName, String EmailAddress, String NextPage, String NextPageLinkName)
+		{
+			FormSubmission form = new FormSubmission();
+			form.CustomerName = CustomerName;
+			form.ReferringUrl = ReferringUrl;
+			form.FormName = FormName;
+			form.PageName = PageName;
+			form.FirstName = FirstName;
+			form.LastName = LastName;
+			form.EmailAddress = EmailAddress;
+			form.NextPage = NextPage;
+			form.NextPageLinkName = NextPageLinkName;
+			form.Date = DateTime.Now;
+			try
+			{
+				dc.FormSubmissions.InsertOnSubmit(form);
+				dc.SubmitChanges();
+			}
+			catch (Exception e)
+			{
+			}
+		}
+	}
+}
